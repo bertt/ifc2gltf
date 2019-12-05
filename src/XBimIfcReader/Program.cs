11ad5fbe878492625f88e7e9a5da9281cdcf538e
Bert Temme <btemme@hotmail.com>
2019-12-05 12:57:32 +0100

    add convert to gltf

--- src/XBimIfcReader/Program.cs
+++ src/XBimIfcReader/Program.cs
@@ -6,6 +6,12 @@ using Xbim.ModelGeometry.Scene;
 using System;
 using System.Linq;
 using Xbim.Ifc4.Kernel;
+using SharpGLTF.Geometry;
+using VERTEX = SharpGLTF.Geometry.VertexTypes.VertexPosition;
+using System.Threading;
+using SharpGLTF.Materials;
+using System.Numerics;
+using System.Diagnostics;
 
 namespace ConsoleApp7
 {
@@ -16,9 +22,69 @@ namespace ConsoleApp7
             const string file = @"spaces_all.ifc";
             var model = IfcStore.Open(file);
 
-            var ifcProject = model.FederatedInstances.OfType<IfcProject>().FirstOrDefault();
+            // PrintSemantics(model);
 
-            
+            // read geometries...
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var context = new Xbim3DModelContext(model);
+            context.CreateContext();
+            var geoms = context.ShapeGeometries().ToList();
+            Console.WriteLine("Geometries:" + geoms.Count);
+
+            var mesh = new MeshBuilder<VERTEX>("mesh");
+            var material1 = new MaterialBuilder()
+               .WithDoubleSide(true)
+               .WithMetallicRoughnessShader()
+               .WithChannelParam("BaseColor", new Vector4(1, 0, 0, 1));
+
+
+            foreach (var geom in geoms)
+            {
+                var data = ((IXbimShapeGeometryData)geom).ShapeData;
+
+                using (var stream = new MemoryStream(data))
+                {
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        var meshBim = reader.ReadShapeTriangulation();
+                        // Console.WriteLine("Faces: " + meshBim.Faces.Count + " vertices: " + meshBim.Vertices.Count);
+
+                        foreach (var face in meshBim.Faces)
+                        {
+                            var indeces = face.Indices;
+
+                            for (var triangle = 0; triangle < face.TriangleCount; triangle++)
+                            {
+                                var start = triangle * 3;
+                                var p0 = meshBim.Vertices[indeces[start]];
+                                var p1 = meshBim.Vertices[indeces[start + 1]];
+                                var p2 = meshBim.Vertices[indeces[start + 2]];
+
+                                var prim = mesh.UsePrimitive(material1);
+                                prim.AddTriangle(
+                                    new VERTEX((float)p0.X, (float)p0.Y, (float)p0.Z * 10),
+                                    new VERTEX((float)p1.X, (float)p1.Y, (float)p1.Z * 10),
+                                    new VERTEX((float)p2.X, (float)p2.Y, (float)p2.Z * 10));
+                            }
+                        }
+                    }
+                }
+            }
+
+            var scene = new SharpGLTF.Scenes.SceneBuilder();
+            scene.AddMesh(mesh, Matrix4x4.Identity);
+            var gltfmodel = scene.ToSchema2();
+            gltfmodel.SaveGLB("mesh.glb");
+            gltfmodel.SaveGLTF("mesh.gltf");
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed: " + stopwatch.Elapsed);
+        }
+
+        private static void PrintSemantics(IfcStore model)
+        {
+            var ifcProject = model.FederatedInstances.OfType<IfcProject>().FirstOrDefault();
             // print buildings, site, storeys, spaces...
             Console.WriteLine("Buildings: " + ifcProject.Buildings.Count());
             var building = ifcProject.Buildings.FirstOrDefault();
@@ -28,37 +94,15 @@ namespace ConsoleApp7
             Console.WriteLine("Location: " + site.RefLongitude.Value.AsDouble + ", " + site.RefLatitude.Value.AsDouble);
 
             Console.WriteLine("storeys: " + storeys.Count());
-            foreach(var storey in storeys)
+            foreach (var storey in storeys)
             {
                 Console.WriteLine(storey.LongName);
 
-                foreach(var space in storey.Spaces)
+                foreach (var space in storey.Spaces)
                 {
                     Console.WriteLine(space.LongName);
                 }
             }
-
-            // read geometries...
-            var context = new Xbim3DModelContext(model);
-            context.CreateContext();
-            var geoms = context.ShapeGeometries().ToList();
-            Console.WriteLine("Geometries:" + geoms.Count);
-            foreach(var geom in geoms)
-            {
-                var data = ((IXbimShapeGeometryData)geom).ShapeData;
-
-                using (var stream = new MemoryStream(data))
-                {
-                    using (var reader = new BinaryReader(stream))
-                    {
-                        var mesh = reader.ReadShapeTriangulation();
-                        Console.WriteLine("Faces: " + mesh.Faces.Count + " vertices: " + mesh.Vertices.Count);
-
-                        // todo: create glTF...
-                    }
-                }
-            }
-
         }
     }
 }
