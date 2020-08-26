e459ffbe9fa49c1fa4d93c1557a5808c2bf5ecdc
bertt <btemme@hotmail.com>
2020-08-26 16:04:44 +0200

    attempt to read office.ifc

--- src/ifc2gltf/GltfConverter.cs
+++ src/ifc2gltf/GltfConverter.cs
@@ -1,13 +1,14 @@
 ﻿using SharpGLTF.Geometry;
 using SharpGLTF.Materials;
 using SharpGLTF.Scenes;
-using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
 using Xbim.Common.Geometry;
 using Xbim.Common.XbimExtensions;
 using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
 using Xbim.ModelGeometry.Scene;
 using VERTEX = SharpGLTF.Geometry.VertexTypes.VertexPosition;
 
@@ -15,12 +16,54 @@ namespace ifc2gltf
 {
     public static class GltfConverter
     {
+        private static XbimShapeTriangulation GetMeshes (Xbim3DModelContext context, IIfcProduct product)
+        {
+            XbimShapeTriangulation ifcMesh = null;;
+            var productShape =
+                context.ShapeInstancesOf(product)
+                    .Where(p => p.RepresentationType != XbimGeometryRepresentationType.OpeningsAndAdditionsExcluded)
+                .Distinct();
+
+            if (productShape.Any())
+            {
+                var shapeInstance = productShape.FirstOrDefault();
+                var shapeGeometry = context.ShapeGeometry(shapeInstance.ShapeGeometryLabel);
+
+                byte[] data = ((IXbimShapeGeometryData)shapeGeometry).ShapeData;
+
+                //If you want to get all the faces and triangulation use this
+                using (var stream = new MemoryStream(data))
+                {
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        ifcMesh = reader.ReadShapeTriangulation();
+                    }
+                }
+            }
+
+            return ifcMesh;
+        }
+
         public static SceneBuilder ToGltf(IfcStore model)
         {
             var context = new Xbim3DModelContext(model);
             context.CreateContext();
-            var geoms = context.ShapeGeometries().ToList();
-            Console.WriteLine("Number of geometries: " + geoms.Count);
+
+            var ifcProject = model.Instances.OfType<IIfcProject>().FirstOrDefault();
+            var ifcSite = model.Instances.OfType<IIfcSite>().FirstOrDefault();
+            var ifcBuilding = ifcSite.Buildings.FirstOrDefault();
+            var ifcStoreys = ifcBuilding.BuildingStoreys; // 3 in case of office
+
+            var spaceMeshes = new List<XbimShapeTriangulation>();
+            foreach(var storey in ifcStoreys)
+            {
+                var spaces = storey.Spaces;
+                foreach(var space in spaces)
+                {
+                    var ifcmesh= GetMeshes(context, space);
+                    spaceMeshes.Add(ifcmesh);
+                }
+            }
 
             var mesh = new MeshBuilder<VERTEX>("mesh");
             var material1 = new MaterialBuilder()
@@ -29,13 +72,11 @@ namespace ifc2gltf
                .WithChannelParam("BaseColor", new Vector4(1, 0, 0, 1));
 
 
-            foreach (var geom in geoms)
+            foreach (var meshBim in spaceMeshes)
             {
-                var data = ((IXbimShapeGeometryData)geom).ShapeData;
 
-                using var stream = new MemoryStream(data);
-                using var reader = new BinaryReader(stream);
-                var meshBim = reader.ReadShapeTriangulation();
+                var faces = (List<XbimFaceTriangulation>)meshBim.Faces;
+                var vertices = (List<XbimPoint3D>)meshBim.Vertices;
 
                 foreach (var face in meshBim.Faces)
                 {
@@ -56,7 +97,6 @@ namespace ifc2gltf
                     }
                 }
             }
-
 
             var scene = new SceneBuilder();
             scene.AddRigidMesh(mesh, Matrix4x4.Identity);
